547e9849f2b3471d878056d05579f063fca965b2
davilemos138-ux <davilemos138@gmail.com>
2026-07-02 20:48:41 -0300

    sistema de vendas

--- sistema_vendas.py
+++ sistema_vendas.py
@@ -37,12 +37,32 @@ while True:
             if qtd_produto <= 0:
                 print("Quantidade inválida!")
             elif qtd_produto <= estoque_produtos[id_produto]["quantidade"]:
-                carrinho.append(estoque_produtos[id_produto])
-                estoque_produtos[id_produto]["quantidade"] -= qtd_produto
+               item = {
+                   "qtd": qtd_produto,
+                   "nome": estoque_produtos[id_produto]["nome"],
+                   "preco": estoque_produtos[id_produto]["preco"],
+                   "preco_total": qtd_produto * estoque_produtos[id_produto]["preco"]
+               }
+               carrinho.append(item)
+               estoque_produtos[id_produto]["quantidade"] -= qtd_produto
+               print(item)
+            else:
+                print(f"Quantidade indisponível, temos apenas {estoque_produtos[id_produto]["quantidade"]} no estoque.")
+
+        else:
+            print("ID informado não existe no estoque!")
+
 
 
     elif opcao == 3:
-        print("Visualizando carrinho")
+        if carrinho:
+
+            print("Visualizando carrinho")
+            subtotal = 0
+            for i in carrinho:
+                print(f"{i["qtd"]}x {i["nome"]} no valor R$ {i["preco"]}(cada)\nTotal R${i["preco_total"]} ")
+                subtotal += i["preco_total"]
+            print(f"Subtotal da Compra R${subtotal}")
 
     elif opcao == 4:
         print("Finalizando Compra")
